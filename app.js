3d918f3a48575c46a457481e9e9eebdbf28b3beb
chen <chenpeng8i3@foxmail.com>
2017-06-20 23:20:28 +0800

    mongodb

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
+var mongoose = require("./db/db");
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -19,29 +22,51 @@ app.set('view engine', 'html');
 app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public'), {index: false}));
+app.use(session({
+    secret: 'aws-blog',
+    name: 'aws-blog',
+    saveUninitialized: false, // don't create session until something stored
+    resave: false,//don't save session if unmodified
+    unset: 'destroy',//The session will be destroyed (deleted) when the response ends.
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection
+    })
+}));
 
 app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 module.exports = app;
