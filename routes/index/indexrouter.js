eb388e5007518a1abc23c45984a5fae37b5373aa
chem <846869169@qq.com>
2017-06-21 15:59:15 +0800

    nav-bar路由

--- routes/index/indexrouter.js
+++ routes/index/indexrouter.js
@@ -3,14 +3,25 @@
  */
 var express = require('express');
 var router = express.Router();
-var moment = require('moment');
 
 router.get('/', function (req, res) {
     res.render('welcome', {"title": "Welcome"});
 });
 
 router.get('/index', function (req, res) {
-    res.render('index', {"title": "Index"});
+    res.render('index', {"title": "Index", "url": "/pages/my-blog.html"});
+});
+
+router.get('/profile', function (req, res) {
+    res.render('index', {"title": "Profile", "url": "/pages/my-profile.html"});
+});
+
+router.get('/tool', function (req, res) {
+    res.render('index', {"title": "Tool", "url": "/pages/my-tool.html"});
+});
+
+router.get('/photo', function (req, res) {
+    res.render('index', {"title": "Photo", "url": "/pages/my-photo.html"});
 });
 
 module.exports = router;
