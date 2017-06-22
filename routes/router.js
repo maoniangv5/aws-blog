366760517a5a7f4105c0781e851de41d00e91146
chem <846869169@qq.com>
2017-06-22 18:21:59 +0800

    图片上传

--- routes/router.js
+++ routes/router.js
@@ -4,10 +4,13 @@ var index = require('./index/indexrouter');
 var users = require('./admin/userrouter');
 var category = require('./admin/categoryrouter');
 var wechat = require('./wechat/wechat')
+var fileupload = require('./fileupload/fileupload');
+
 
 router.use('/', index);
 router.use('/admin', users);
 router.use('/wechat', wechat);
 router.use('/api/category', category);
+router.use('/api/fileupload', fileupload);
 
 module.exports = router;
