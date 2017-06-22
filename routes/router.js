576a1e1efe7ad8722d52688cdeab69cd76d2a5c3
chem <846869169@qq.com>
2017-06-22 13:42:20 +0800

    wechat测试

--- routes/router.js
+++ routes/router.js
@@ -3,9 +3,11 @@ var router = express.Router();
 var index = require('./index/indexrouter');
 var users = require('./admin/userrouter');
 var category = require('./admin/categoryrouter');
+var wechat = require('./wechat/wechat')
 
 router.use('/', index);
 router.use('/admin', users);
+router.use('/wechat', wechat);
 router.use('/api/category', category);
 
 module.exports = router;
