95f33537db0dd1d397b0d851fad98fc77538197f
chem <846869169@qq.com>
2017-06-23 17:58:52 +0800

    图片压缩

--- routes/index/indexrouter.js
+++ routes/index/indexrouter.js
@@ -5,6 +5,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
+    console.log(req.headers)
     res.render('welcome', {"title": "Welcome"});
 });
 
