2d0a121555d6592447ba6d5e948d9e4eef0617ff
chen <chenpeng8i3@foxmail.com>
2017-06-25 04:24:05 +0800

    popover 未解决

--- routes/file/fileuploadrouter.js
+++ routes/file/fileuploadrouter.js
@@ -16,20 +16,20 @@ var mwMulter1 = multer({
     dest: imgs_dir
 });
 
-router.post('/img', mwMulter1, function (req, res, next) { // 上传图片API
+router.post('/img', mwMulter1.any('files'), function (req, res, next) { // 上传图片API
     if (!fs.existsSync(imgs_dir)) {
         fs.mkdirSync(imgs_dir);
     }
     var rm = new RestMsg();
     var files = req.files;
     var query = {};
-    if (files.file) {
+    if (files[0]) {
 
-        fs.readFile(files.file.path, function (err, buf) {
-            var ext = files.file.extension
-            var old_path = files.file.path
+        fs.readFile(files[0].path, function (err, buf) {
+            var ext = files[0].mimetype.split('/')[files[0].mimetype.split('/').length - 1]
+            var old_path = files[0].path
             query.md5 = md5(buf); // 调用md5包，计算图片md5值
-            query.name = files.file.originalname;
+            query.name = files[0].originalname;
             query.path = imgs_dir + '/' + md5(buf) + '.' + ext;
             query.url = imgs_url + '/' + md5(buf) + '.' + ext;
 
@@ -49,12 +49,12 @@ router.post('/img', mwMulter1, function (req, res, next) { // 上传图片API
                     var desc = query.name.split('.');
                     desc.pop();
                     var obj = {
-                        "title":query.name,
-                        "url":query.url,
-                        "type":"original",
-                        "md5":query.md5,
-                        "is_pub":true,
-                        "desc":desc.join('.')
+                        "title": query.name,
+                        "url": query.url,
+                        "type": "original",
+                        "md5": query.md5,
+                        "is_pub": true,
+                        "desc": desc.join('.')
                     }
                     ImageService.save(obj, function (err, bo) {
                         if (err) {
@@ -66,7 +66,7 @@ router.post('/img', mwMulter1, function (req, res, next) { // 上传图片API
                         res.send(rm);
                     })
                 } else {
-                    ImageService.findOne({"md5": query.md5},function (err, bo) {
+                    ImageService.findOne({"md5": query.md5}, function (err, bo) {
                         if (err) {
                             rm.errorMsg(err);
                             return res.send(rm)
