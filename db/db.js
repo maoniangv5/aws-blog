a50e2561fdc7ba92d9a23379d8122b869f31f5e1
chem <846869169@qq.com>
2017-06-26 13:12:17 +0800

    mongose useMongoClient

--- db/db.js
+++ db/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/aws-blog');
+mongoose.connect('mongodb://localhost/aws-blog', {useMongoClient: true});
 
 module.exports = mongoose;
